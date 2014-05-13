e970d4f29c6b8e47cc26487e9f6f5048086da7c8
Serhii Kilinich <orf1888@gmail.com>
2014-05-13 22:19:54 +0200

    simple JUnit test

---
+++ src/test/java/model/WalutaTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class WalutaTest
+{
+
+	@Test
+	public void testKonstruktor()
+	{
+		Waluta w = new Waluta(42, "Eurogbki");
+		assertEquals(42, w.getId());
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void testNullValue()
+	{
+		new Waluta(42, null);
+	}
+}
