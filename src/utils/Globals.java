5df854603cca3251ea9170cf20be7d35e53ac982
Serhii Kilinich <orf1888@gmail.com>
2014-05-13 21:07:25 +0200

    app version

--- src/utils/Globals.java
+++ src/utils/Globals.java
@@ -1,9 +1,27 @@
 package utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.swing.ImageIcon;
 
 public class Globals
 {
+	static
+	{
+		Properties prop = new Properties();
+		try (InputStream in = Globals.class.getResourceAsStream("version"))
+		{
+			prop.load(in);
+		} catch (IOException e)
+		{
+			throw new RuntimeException(e);
+
+		}
+		WersjaAplikacji = "Subiekt GTC v." + prop.getProperty("vesion");
+	}
+
 	public final static boolean Widoczny = true;
 
 	public final static boolean Niewidoczny = false;
@@ -14,7 +32,7 @@ public class Globals
 
 	public final static boolean SzukajRownaSie = false;
 
-	public final static String WersjaAplikacji = "Subiekt GTC v.1.15.58";
+	public final static String WersjaAplikacji;
 
 	public final static ImageIcon IkonaAplikacji = new ImageIcon(Globals.class
 			.getClassLoader().getResource("sGTC.gif"));
