9f0d8331af9c606482195f2dba11c3bb6948d353
Serhii Kilinich <orf1888@gmail.com>
2014-05-21 15:52:17 +0200

    update updater

--- src/main/java/kontroler/UpdateProgressKontroler.java
+++ src/main/java/kontroler/UpdateProgressKontroler.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 import utils.Globals;
+import utils.MojeUtils;
 import widok.UpdateProgress;
 
 public class UpdateProgressKontroler extends Thread
@@ -20,12 +21,13 @@ public class UpdateProgressKontroler extends Thread
 	@Override
 	public void run()
 	{
+		int i = 0, exitStatus;
 		try
 		{
-			String[] cmdLine = new String[]
-			{ "cmd", "/c", Globals.UpdateBat };
-			System.err.println(cmdLine[2]);
-			ProcessBuilder builder = new ProcessBuilder(cmdLine);
+			/* Update repository */
+			String[] cmdLineGitPull = new String[]
+			{ "cmd", "/c", Globals.gitPullBat };
+			ProcessBuilder builder = new ProcessBuilder(cmdLineGitPull);
 			builder.redirectError(new File("nul"));
 			Process child = builder.start();
 			child.getOutputStream().close();
@@ -33,7 +35,6 @@ public class UpdateProgressKontroler extends Thread
 			Reader reader = new InputStreamReader(child.getInputStream());
 			try (BufferedReader bufferedReader = new BufferedReader(reader))
 			{
-				int i = 0;
 				String line;
 				while ((line = bufferedReader.readLine()) != null)
 				{
@@ -44,9 +45,48 @@ public class UpdateProgressKontroler extends Thread
 					{
 						up.setLblStatus("Update repozytorium...");
 					}
+					if (line.contains("Already up-to-date."))
+					{
+						up.dispose();
+						reader.close();
+						MojeUtils
+								.showMsg("Dana instatncja \"Subiekt GTC\" jest aktualna!");
+						return;
+					}
+
+				}
+				reader.close();
+			}
+			try
+			{
+				exitStatus = child.waitFor();
+			} catch (InterruptedException e)
+			{
+				throw new IOException(e);
+			}
+			if (exitStatus != 0)
+				throw new IOException("Błąd update'u repozytorium! "
+						+ exitStatus);
+
+			/* Maven verify */
+			String[] cmdLineMvnVerify = new String[]
+			{ "cmd", "/c", Globals.mavenVerifyBat };
+			builder = new ProcessBuilder(cmdLineMvnVerify);
+			builder.redirectError(new File("nul"));
+			child = builder.start();
+			child.getOutputStream().close();
+			reader = new InputStreamReader(child.getInputStream());
+			try (BufferedReader bufferedReader = new BufferedReader(reader))
+			{
+				String line;
+				while ((line = bufferedReader.readLine()) != null)
+				{
+
+					System.err.println(++i + " " + line);
+					up.setProgress(i);
 					if (line.contains("Scanning for projects..."))
 					{
-						up.setLblStatus("Kompilacja Subiekt GTC...");
+						up.setLblStatus("Kompilacja \"Subiekt GTC\"...");
 					}
 					if (line.contains("T E S T S"))
 					{
@@ -56,15 +96,18 @@ public class UpdateProgressKontroler extends Thread
 					{
 						up.setLblStatus("Ponowne uruchomienie...");
 					}
-					if (line.contains("Koniec!"))
+					if (line.contains("BUILD FAILURE"))
 					{
+						up.dispose();
 						reader.close();
-						System.exit(0);
+						MojeUtils
+								.showMsg("Błąd budowania \"Subiekt GTC\".\nTen problem należy niezwłocznie zgłosić!");
+						return;
 					}
-				}
-			}
 
-			int exitStatus;
+				}
+				reader.close();
+			}
 			try
 			{
 				exitStatus = child.waitFor();
@@ -73,8 +116,28 @@ public class UpdateProgressKontroler extends Thread
 				throw new IOException(e);
 			}
 			if (exitStatus != 0)
-				throw new IOException("rpm failed with exit status "
-						+ exitStatus);
+				throw new IOException("Błąd budowania! " + exitStatus);
+
+			/* Replace Subiekt GTC instace */
+			String[] cmdLineReplace = new String[]
+			{ "cmd", "/c", Globals.replaceBat };
+			builder = new ProcessBuilder(cmdLineReplace);
+			builder.redirectError(new File("nul"));
+			child = builder.start();
+			child.getOutputStream().close();
+
+			System.exit(0);
+
+			try
+			{
+				exitStatus = child.waitFor();
+			} catch (InterruptedException e)
+			{
+				throw new IOException(e);
+			}
+			if (exitStatus != 0)
+				throw new IOException("Błąd podmiany! " + exitStatus);
+
 		} catch (Exception e)
 		{
 			throw new RuntimeException(e);
